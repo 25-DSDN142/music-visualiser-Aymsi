6a8d13c93ffd167322f7b00cdae08eb1a2500c1e
Aymsi <agknott23@gmail.com>
2025-08-14 13:30:03 +1200

    learning! playing with feeding perameters of function into variables in code so that the shape directly interacts with the song

--- music_dayone.js
+++ music_dayone.js
@@ -1,17 +1,20 @@
-let circleY = 350
+let circleY = 350;
+let circleSize;
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   background(20)
+  //basing scale of circle width on drums of song
+circleSize = drum*4;
 
   fill (255, 171, 216); //light pink
-  ellipse(300,circleY,50);
+  ellipse(300,circleY, circleSize);
   
   circleY = circleY + 1
 
   //adding if statement for loop
   //if it reaches the bottom of the page, make it go back to the top of the page
-  if (circleY > 100) {
-   sunY = 0
+  if (circleY > 1000) {
+   circleY = 0
   }
 }
