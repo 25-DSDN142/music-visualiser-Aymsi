a21f7325ae317a4f4cac5ab010ef0c914b85f10f
Aymsi <agknott23@gmail.com>
2025-09-19 11:32:56 +1200

    implementing rotation!

--- music_dayone.js
+++ music_dayone.js
@@ -2,6 +2,7 @@ let circleY = 350;
 let circleSize;
 
 let smooth = 1; //consistency for scale 
+let rotationAngle = 0 //base angle no rotation
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
@@ -22,13 +23,17 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   // circleY = 0
 // }
 
+angleMode(RADIANS);
 //idea behind this: scale changing dependant on quiet vs loud drum
 //map(drum, minimum drum range, *max drum range to hit edge on loudest part*, 0.25 of full 1000,1000 res star shape being 1, 4 being largest size reaching 1000,1000 edge of canvas)
 let scaleFactor = map(drum, 50, 100, 1, 4); //drum input to scale range
 
+let starSpin = map(vocal, 0, 1, 0, TWO_PI);
+
 smooth = lerp(smooth, scaleFactor, 0.35); //adding smoothing to make scaling not jittery
 
 translate(500,500);
+rotate(starSpin);
 scale(smooth);
 
 beginShape();
